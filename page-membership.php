fdccc1d5a324546c09b92d174e2d87e0aacd972b
Hubert Cep <hubert.cep@elitecrew.io>
2022-10-01 09:22:31 +0200

    First commit

---
+++ page-membership.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Membership Theme
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Fancy Lab
+ */
+
+get_header();
+?>
+		<div class="content-area">
+			<main>
+				<div class="container mx-auto">
+					<div class="row">
+						<?php the_content() ;?>
+                        
+					</div>
+				</div>
+			</main>
+		</div>
+<?php get_footer(); ?>
